021dadb74a8c0768d53cd6f0418755c4b30cb177
wzg12345 <854933521@qq.com>
2020-04-20 16:57:16 +0800

    20200420

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -133,7 +133,8 @@
             getQueryString(name){
                 var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
                 var r = window.location.search.substr(1).match(reg);  //匹配目标参数
-                if (r != null) return unescape(r[2]); return null; //返回参数值
+                if (r != null) return unescape(r[2]);
+                return null; //返回参数值
             },
             //微信获取签名信息等
             onReadyCogin() {
